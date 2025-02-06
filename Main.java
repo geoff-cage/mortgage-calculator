768777c4b07a8a380f4e59b293515726037cd517
Geoff-cage <geoffreykaijage1@gmail.com>
2025-02-07 00:13:52 +0300

    data handling

--- Main.java
+++ Main.java
@@ -8,21 +8,40 @@ public class Main {
     public static void main(String[] args) {
           int monthInYears = 12;
         int percentage= 100;
-        //float price=  38.88F;
-//        Date now = new Date();
-        String message = "Principal:";
-        System.out.print(message);
+        int principal = 0;
+        float rate =0;
+        int period =0;
+        float newRate = 0;
+        String message = "Principal (100,000 - 1,000,000,0000):";
         Scanner scanner = new Scanner(System.in);
-        int principal = scanner.nextInt();
+        while(true) {
+            System.out.print(message);
+            principal = scanner.nextInt();
+            if(principal >=100000 && principal<=1000000)
+                break;
+            System.out.println("Enter value between 100,000 and 1,000,000");
+        }
 
         String information = "Rate:";
-        System.out.print(information);
-        float rate = scanner.nextFloat();
-        float newRate = (rate/percentage)/monthInYears;
+        while (true) {
+            System.out.print(information);
+             rate = scanner.nextFloat();
+             if (rate >=1 && rate<=30) {
+                 newRate = (rate / percentage) / monthInYears;
+                 break;
+             }
+            System.out.println("Enter value greater than or equal to 1 and less than or equal to 30");
+        }
+
 
         String info = "Period:";
-        System.out.print(info);
-        int period = scanner.nextInt();
+        while(true) {
+            System.out.print(info);
+            period = scanner.nextInt();
+            if(period>0 && period<=30)
+                break;
+            System.out.println("Enter value greater than 0 and less than or equal 30");
+        }
         int numberOfPayments = period * monthInYears;
 
 
