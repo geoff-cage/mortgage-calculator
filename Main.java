83f7e0ad12d2d48656a9188b8da69adfbf8bbdfa
Geoff-cage <geoffreykaijage1@gmail.com>
2025-03-14 21:58:46 +0300

    formula refactoring

--- Main.java
+++ Main.java
@@ -6,13 +6,11 @@ import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
-          int monthInYears = 12;
-        int percentage= 100;
+
         int principal = 0;
         float rate =0;
         int period =0;
-        float newRate = 0;
-        int numberOfPayments=0;
+
         String message = "Principal (100,000 - 1,000,000,0000):";
         Scanner scanner = new Scanner(System.in);
         while(true) {
@@ -27,39 +25,34 @@ public class Main {
         while (true) {
             System.out.print(information);
              rate = scanner.nextFloat();
-             if (rate >=1 && rate<=30) {
-                 newRate = (rate / percentage) / monthInYears;
+             if (rate >=1 && rate<=30)
                  break;
-             }
             System.out.println("Enter value greater than or equal to 1 and less than or equal to 30");
         }
-
 
         String info = "Period:";
         while(true) {
             System.out.print(info);
             period = scanner.nextInt();
-            if(period>0 && period<=30) {
-                numberOfPayments = period * monthInYears;
+            if(period>0 && period<=30)
                 break;
-            }
             System.out.println("Enter value greater than 0 and less than or equal 30");
         }
 
 
-
-//        initial method for calculation
-//        float upperBracket = (float)Math.pow((1+newRate),numberOfPayments)* newRate;
-//        float lowerBracket = (float)Math.pow((1 + newRate),numberOfPayments)- 1;
-//        float mortgage = principal *(upperBracket/lowerBracket);
-
-        float mortgage = (float) (principal * (newRate*(Math.pow((1+newRate),numberOfPayments)))/(Math.pow((1+newRate),numberOfPayments)- 1));
+        float mortgage= calculateMortgage(principal,rate,period);
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);
         System.out.println("your mortgage is:" + mortgageFormatted);
 
-      // initial method of printing
-      //  System.out.println("you're principal is:" + principal + " and rate is:" + newRate + " and period is: "+period +" and your mortgage is:" + mortgage+"$");
 
+    }
+    public static float calculateMortgage(int principal,float rate, int period){
+        int monthInYears = 12;
+        int percentage= 100;
+       short numberOfPayments = (short) (period * monthInYears);
+        float newRate = (rate / percentage) / monthInYears;
+        float mortgage = (float) (principal * (newRate*(Math.pow((1+newRate),numberOfPayments)))/(Math.pow((1+newRate),numberOfPayments)- 1));
+        return mortgage;
     }
 }
